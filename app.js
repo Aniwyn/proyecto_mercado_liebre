30ff7751c173dbb0fe1c5a99fe2a243ada31cfd9
Aniwyn <kanashiiguree@gmail.com>
2023-08-07 19:36:25 -0300

    Actualizacion de animaciones

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, "./views/home.html"))
 })
 
-app.listen(8080, () => {
-    console.log('[server]: corriendo en el puerto 8080.');
+app.get('/register', (request, response) => {
+    response.sendFile(path.join(__dirname, "./views/register.html"))
+})
+
+app.get('/login', (request, response) => {
+    response.sendFile(path.join(__dirname, "./views/login.html"))
+})
+
+// app.listen(8080, () => {
+//     console.log('[server]: running at port 8080 (http://localhost:8080/).');
+// })
+
+const port = process.env.port || 8080;
+app.listen(port, () => {
+    console.log(`[server]: running at port $[port] (http://localhost:$[port]/).`);
 })
